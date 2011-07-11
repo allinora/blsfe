b15d28912624c9d1a0d260abf6dfeba6d830fb72
Atif Ghaffar <atif@tipi-hq.com>
2011-07-11 09:53:34 +0200

    Adding a todo file

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 /** Configuration Variables **/
 
-define ('DEVELOPMENT_ENVIRONMENT',true);
+define ('DEVELOPMENT_ENVIRONMENT',false);
 
 define('BLSERVER_URL','http://localhost/tipibls');
 define('BLSERVER_SHARED_KEY','rel0aded');
