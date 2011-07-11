15e5a6d299629c29fafbd25556436ecab47dcc19
Atif Ghaffar <atif@tipi-hq.com>
2011-07-11 12:03:17 +0200

    WTF is up with the indentation

--- library/blmodel.class.php
+++ library/blmodel.class.php
@@ -1,14 +1,14 @@
 <?php
 class BLModel extends BLQuery {
 	protected $_model;
-    protected $_idField;
-    protected $_searchField;
+	protected $_idField;
+	protected $_searchField;
 
 	function __construct($endpoint, $_idF="id", $_searchF) {
 		parent::__construct();
 		$this->_model = $endpoint;
-        $this->_idField=$_idF;
-        $this->_searchField=$_searchF;
+		$this->_idField=$_idF;
+		$this->_searchField=$_searchF;
 	}
 
 
