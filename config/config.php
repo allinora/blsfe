df83a2c15bd590e0b392af4540d8a3cdb7eb9ad3
Atif Ghaffar <atif@tipi-hq.com>
2011-07-11 10:00:48 +0200

    Removing config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/** Configuration Variables **/
-
-define ('DEVELOPMENT_ENVIRONMENT',false);
-
-define('BASE_URL','/blsfe');
-define('BLSERVER_URL','http://localhost/tipibls');
-define('BLSERVER_SHARED_KEY','rel0aded');
