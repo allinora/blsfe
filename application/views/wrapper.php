8aa8ecb615f52bef262840452f7630ad0319fb52
Atif Ghaffar <atif@tipi-hq.com>
2011-07-11 10:00:26 +0200

    Adding the config.template.php. must remove config.php

--- application/views/wrapper.php
+++ application/views/wrapper.php
@@ -1,3 +1,9 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>/css/default.css" />
+</head>
+<body>
 <h1>Header</h1>
 <?php echo $content ?>
 <h1>Footer</h1>
+</body>
