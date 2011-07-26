dd823c95bce4cb1d2d556279c1d3129d1c193ca7
Atif Ghaffar <atif@tipi-hq.com>
2011-07-26 18:53:07 +0200

    Updating the skel

--- bin/skel/config/config.php
+++ bin/skel/config/config.php
@@ -1,9 +1,7 @@
 <?php
+ini_set("date.timezone", 'Europe/Berlin');
 
 /** Configuration Variables **/
-
-// Set the date.timezone to your timezone
-ini_set("date.timezone", 'Europe/Berlin');
 
 define ('DEVELOPMENT_ENVIRONMENT',true);
 
@@ -17,3 +15,37 @@ define('CACHE_PATH', ROOT . DS . "tmp" . DS . "cache");
 
 define('SESSION_BACKEND', 'FILE');
 define('SESSION_PATH', ROOT . DS . "tmp" . DS . "sessions");
+
+//language related
+define('LANGUAGES', 'en|de|fr');
+define('DATE_FORMAT_EN', "n-j-Y, H:i:s");
+define('DATE_FORMAT_FR', "j/n/Y, H:i:s");
+define('DATE_FORMAT_DE', "j/n/Y, H:i:s");
+
+define('TEMPLATE_BACKEND', "Smarty");
+
+
+//smarty options
+
+// Path to Smarty.class.php
+define('SMARTY_LIBRARY', ROOT.DS."library".DS."3rdparty".DS."smarty".DS."Smarty-3.0.8".DS."libs".DS."Smarty.class.php");
+
+// Default template directory 
+define('SMARTY_TEMPLATE_DIR', ROOT.DS."application".DS."views");
+
+
+define('SMARTY_COMPILE_DIR', ROOT.DS."tmp".DS."smarty_compile");
+define('SMARTY_CACHE_DIR', ROOT.DS."tmp".DS."smarty_cache");
+
+// YOUR smarty plugins directory. This will be added to the list of smarty plugins
+define('SMARTY_PLUGINS_DIR', ROOT.DS."library".DS."smarty-plugins");
+
+// left and right delimeters for smarty tags. we recommend <{ $something }>
+define('SMARTY_LEFT_DELIMETER', "<{");
+define('SMARTY_RIGHT_DELIMETER', "}>");
+
+
+//stuff related to tipiness v4->v5
+define('MEDIA_JS_PREFIX', "/staticmedia");
+define('MEDIA_CSS_PREFIX', "/staticmedia");
+define('ROBOTS', false);
