b68ea580e2f1f9ca0dfdd2dfa1892f4f6c90da9a
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-10-12 14:56:56 +0200

    Happy for now... Lets test this in action

--- library/blform.class.php
+++ library/blform.class.php
@@ -1,6 +1,7 @@
 <?php
 include_once(dirname(__FILE__) . "/blmodel.class.php");
 
+// These types should match the bls types
 define('OBJ_DTYPE_INT', 1);
 define('OBJ_DTYPE_URL', 2);
 define('OBJ_DTYPE_EMAIL', 3);
@@ -20,38 +21,45 @@ define('OBJ_DTYPE_CURRDATE',15);
 
 class BLForm extends BLModel {
 	var $tray;
-	
-	function __construct($model, $id) {
-		
+	var $vars;
+
+	function __construct($model, $data=array()) {
 		$x=new bltransport();
 		$shadow=$x->callBusinessLogicService($model . "/reflect");
 		$objectProperties = get_object_vars($shadow);
-        $vars=$objectProperties["vars"];
-        $this->tray($vars);
-        //return $this->render($vars);
-		
+        $this->vars=$objectProperties["vars"];
+		//print "<pre>" . print_r($this->vars, true) . "</pre>";
+
+
+		// Fillin data for the vars if provided
+		foreach ($this->vars as $id=> &$var) {
+			if (isset($data[$id])){
+				$var["value"]=$data[$id];
+			}
+		}
 	}
+	
+	function setCss($id, $value){
+		$this->vars[$id]["css"]=$value;
+	}
+	
+	
+	function setupTray(){
+		$this->tray=array();
+		foreach ($this->vars as $id=> $f) {
+			if (in_array($id, array("id", "createtime", "ts"))){ // Ignore these always. They are handled by the system
+				continue;
+			}
+			$this->tray[$id]=$this->setupField($id, $f);
+		}
+	}
+	
 	function getTray(){
 		return $this->tray;
 	}
-	function tray($_vars){
-		$this->tray=array();
-		foreach ($_vars as $id=> $f) {
-			$this->tray[$id]=$this->field($id, $f);
-		}
-	}
 
-	function render($y){
-		$_vars=$y;
-		//print "<pre>" . print_r($_vars->lastname, true) . "</pre>";
-		foreach ($_vars as $id=> $f) {
-			$this->field($id, $f);
-		}
-	}
 
-	function field($id, $field){
-		
-		//print "<pre>" . print_r($z, true) . "</pre>";
+	function setupField($id, $field){
 		switch($field["data_type"]){
 			case OBJ_DTYPE_STRING:	
 			if ($field['maxlength'] > 200){
@@ -72,11 +80,33 @@ class BLForm extends BLModel {
 	}
 	
 	function textbox($id, $f){
-		$text="<input type='text' name='$id' id='id' value=''>";
+		$text="<input type='text' name='$id' id='$id' value='"  . $f["value"].  "'";
+		if ($f["maxlength"]){
+			$text.=" maxlength="  . $f["maxlength"];
+		}
+		if ($f["maxlength"]>50){
+			$text.=" size=50 ";
+		} else {
+			$text.=" size=" . $f["maxlength"] ;
+		}
+		
+		
+		if ($f["css"]){
+			$text.=" style='" . $f["css"] . "'";
+		}
+		$text.=" >";
 		return $text;
 	}
 	function textarea($id, $f){
-		$text="<textarea name='$id' id='id'></textarea>";
+		$text="<textarea name='$id' id='$id' ";
+		if ($f["css"]){
+			$text.="style='" . $f["css"] . "'";
+		}
+		$text.=">";
+		if ($f["value"]){
+			$text.=$f["value"];
+		}
+		$text.="</textarea>";
 		return $text;
 	}
 }
