44551ecb5ebbba469f25f1ffe524df0f0371c9dd
Atif Ghaffar <atif@tipi-hq.com>
2011-10-12 21:22:27 +0200

    Sometimes we try to write cache to a directory. Find a way to raise a warning

--- library/cache/file.class.php
+++ library/cache/file.class.php
@@ -20,9 +20,16 @@ class Cache_File extends Cache {
 			return;
 		}
 		$cache_path=CACHE_PATH . DS .  $key;
+		//print "CachePath is $cache_path<br>";
 		$cache_dir=dirname($cache_path);
+		//print "Cache_dir=$cache_dir<br>";
 		if (!is_dir($cache_dir)){
 			mkdir($cache_dir, 0777, true);
+		}
+		if (is_dir($cache_path)){
+			// Make a warning
+			return;
+			
 		}
 		file_put_contents($cache_path, serialize($value));
 	}
