f7415dc50ceb65bf473aeba671c8c8ab203e136e
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-10-16 00:48:43 +0200

    DEFINED the BLSFE_ROOT

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -5,6 +5,9 @@ if (!defined("DS")) {
 if (!defined("ROOT")) {
 	die("Error: ROOT constant is not defined");
 }
+if (!defined("BLSFE_ROOT")) {
+	define("BLSFE_ROOT", dirname(__FILE__));
+}
 
 ini_set("allow_call_time_pass_reference", true);
 if (!defined("APPCONFIGFILE")) {
