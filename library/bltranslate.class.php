0ba8b68b447f4e61e39d136c359e9bc947b21312
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-10-27 14:49:33 +0200

    Adding stuff to generate and manage poeditor

--- library/bltranslate.class.php
+++ library/bltranslate.class.php
@@ -17,4 +17,55 @@ class BLTranslate extends BLTransport{
 	    }
 	    return "X:" . $string[1];
 	}
+	
+	public function getProjects(){
+	    $projects=$this->callBusinessLogicService("/core/po/string/getProjects");
+		return $projects;
+	}
+	
+	public function search($string, $project="", $lang=""){
+		$_params["q"]=$string;
+		$_params["project"]=$project;
+		$_params["language"]=$lang;
+	    $result=$this->callBusinessLogicService("/core/po/string/search", $_params);
+		return $result;
+		
+	}
+	
+	public function getLanguages(){
+		$languages = explode("|", LANGUAGES);
+		return $languages;
+	}
+	
+	public function searchForm($project=null,$language=null,$string=null){
+		$projects=$this->getProjects();
+		$data=$this->projectList( $projects, $project);
+		print $data;
+		$data=$this->languageList( $this->getLanguages(), $language);
+		print $data;
+	}
+	
+	private function projectList($options, $selected){
+		$data="<select id='project' name='project'>";
+		$data.="<option value=''>Any</option>";
+		foreach($options as $o){
+			$data.="<option value='$o'>$o</option>";
+		}
+		$data.="</select>";
+		return $data;
+	}
+	
+	private function languageList($options, $selected){
+		$data="<select id='language' name='language'>";
+		$data.="<option value=''>Any</option>";
+		$data.="<option value='00'>Source</option>";
+		foreach($options as $o){
+			$data.="<option value='$o'>$o</option>";
+		}
+		$data.="</select>";
+		return $data;
+	}
+	
+	
+	
 }
