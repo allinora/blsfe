f81d993f873a1d47876980f82f23b4879a0a2ee5
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-10-27 12:41:41 +0200

    updating params

--- library/bltranslate.class.php
+++ library/bltranslate.class.php
@@ -5,13 +5,13 @@ class BLTranslate extends BLTransport{
 	var $project;
 	var $lang;
 	
-	public function _construct($lang="00", $project=""){
+	public function __construct($lang="00", $project=""){
 		$this->lang=$lang;
-		$this->poject=$project;
+		$this->project=$project;
 	}
 	
 	public function translate($string){
-	    $translation=$this->callBusinessLogicService("/core/po/string/getTranslation", array("string"=>trim($string[1]), "language"=>$lang, "project"=>$project), "GET");
+	    $translation=$this->callBusinessLogicService("/core/po/string/getTranslation", array("string"=>trim($string[1]), "language"=>$this->lang, "project"=>$this->project), "GET");
 		//print "<pre>" . print_r($translation, true) . "</pre>";exit;
 	    if ($translation) {
 			return $translation["msgstr"];
