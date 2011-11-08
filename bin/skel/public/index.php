28e34c7e6e8a4a5caf9100fd55ea0c7bcf2c329f
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-11-08 13:23:51 +0100

    Elaborating

--- bin/skel/public/index.php
+++ bin/skel/public/index.php
@@ -1,9 +1,57 @@
 <?php	
-
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
-define('BLSFEROOT', '/Users/aghaffar/www/blsfe');
+define('BLSFE_ROOT', '/Users/aghaffar/www/blsfe');
 
-$url = $_GET['url'];
+
+
+// Possible location where we can find the BLSFE library
+$blsfe_locations=array(
+        '/opt/git/blsfe',
+        "/Users/aghaffar/www/blsfe",
+        "/home/tipi/blsfe"
+);
+
+$_blsfe_bootstrap_file=_findBootstrap($blsfe_locations);
+
+foreach($blsfe_locations as $l){
+	if (file_exists($l)){
+    	$_blsfe_bootstrap_file=$l;
+    }
+}
+
+
+
+// Do something before the main Hook is called
+function _app_preHook(){
+}
+
+
+// Do something with the content just before its being printed.
+function _app_contentHook($_content){
+        print $content;
+}
+
+
+
+function _findBootstrap($blsfe_locations){
+	foreach($blsfe_locations as $l){
+		if (file_exists($l)){
+			return $l;
+	    }
+	}
+	
+}
+
+if ($_SERVER["BLSFE_BOOTSTRAP_FILE"]){
+        // This is for development. 
+        if (file_exists($_SERVER["BLSFE_BOOTSTRAP_FILE"])){
+                require_once($_SERVER["BLSFE_BOOTSTRAP_FILE"]);
+        }
+} else {
+        if (file_exists($_blsfe_bootstrap_file)){
+                require_once($_blsfe_bootstrap_file);
+        }
+}
 
 require_once (BLSFEROOT . DS . 'library' . DS . 'bootstrap.php');
