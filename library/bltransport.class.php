7c4a52d129af12bc22856ad7acb7a6113c725a08
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-11-17 15:21:31 +0100

    Implementing local transport to the BLSBE. Getting rid of the requirement for HTTP_Request class

--- library/bltransport.class.php
+++ library/bltransport.class.php
@@ -7,101 +7,136 @@ class BLTransport{
 	}
 
     protected function callBusinessLogicService($service, $request_params=array(), $method="GET", $params=array()){
-	//protected function callBusinessLogicService($service, $arguments){
+		if (defined("BLSEBE_TRANSPORT")){
+			if (BLSEBE_TRANSPORT=="local"){
+				return $this->callBusinessLogicServiceLocal($service, $request_params, $method, $params);
+			} else {
+				return $this->callBusinessLogicServiceHttp($service, $request_params, $method, $params);
+			}
+			
+		} else {
+			return $this->callBusinessLogicServiceHttp($service, $request_params, $method, $params);
+		}
+   }
+   	private function callBusinessLogicServiceLocal($service, $request_params=array(), $method="GET", $params=array()){
         global $_SESSION;
 		if (isset($_SESSION["lang"])){
         	$request_params["lang"]=$_SESSION["lang"]; // Multilanguage stuff if available
 		}
    
-        $action_url=BLSERVER_URL . "/" . $service;
-        $params["request_data"]=$request_params;
+		$_ENV["PROJECT_HOME"]=BLSBE_PROJECT_HOME;
+		$_blsbe_bootstrap_file=BLSBE_BOOTSTRAP_FILE;
+		if (file_exists($_blsbe_bootstrap_file)){
+			require_once($_blsbe_bootstrap_file);
+		}
+		$context=array();
+		$context["env"]["REQUEST_METHOD"]="GET";
+		$context["env"]["REQUEST_URI"]=$service;
+		$context["env"]["REMOTE_ADDR"]=$_SERVER["REMOTE_ADDR"];
+		$context["_GET"]=$request_params;
+		$app = new BLS_Router();
+		$res=$app($context);
+		if ($res[0]==200){
+			return unserialize($res[2]);
+		}
+		
+	}
+
+   private function callBusinessLogicServiceHttp($service, $request_params=array(), $method="GET", $params=array()){
+	//protected function callBusinessLogicService($service, $arguments){
+       global $_SESSION;
+		if (isset($_SESSION["lang"])){
+       	$request_params["lang"]=$_SESSION["lang"]; // Multilanguage stuff if available
+		}
+  
+       $action_url=BLSERVER_URL . "/" . $service;
+       $params["request_data"]=$request_params;
 		if (defined("BLSERVER_SHARED_KEY")){
 	        $params["headers"]["X-SHARED-KEY"]=BLSERVER_SHARED_KEY;
 		}
 		//print "<p>Calling $action_url</p><pre>" . print_r($request_params, true) . "</pre>";
-    
-        if ($method=="POST") {
-            $response=$this->postURL($action_url, $params);
-        } else {
-            $response=$this->getURL($action_url, $params);
-        }
-    
-        if (isset($params["no_unserialize"]) && $params["no_unserialize"]) {
-            return $response;
-        }
-    
-        $response = trim($response);
-        $aRet=unserialize($response);
-        	 
-        if( ($aRet === false) && ($response !== serialize(false)) ) {
-        	syslog(LOG_DEBUG, "callBusinessLogicService: ERROR: Could not unserialize response: ".$response);
-        	return false;
-        }
+   
+       if ($method=="POST") {
+           $response=$this->postURL($action_url, $params);
+       } else {
+           $response=$this->getURL($action_url, $params);
+       }
+   
+       if (isset($params["no_unserialize"]) && $params["no_unserialize"]) {
+           return $response;
+       }
+   
+       $response = trim($response);
+       $aRet=unserialize($response);
+       	 
+       if( ($aRet === false) && ($response !== serialize(false)) ) {
+       	syslog(LOG_DEBUG, "callBusinessLogicService: ERROR: Could not unserialize response: ".$response);
+       	return false;
+       }
 
-        return $aRet;
-    }
-
-
-    public function getURL($url, $params=array()){
-        //syslog(LOG_DEBUG, "getURL: $url");
-        require_once("HTTP/Request.php");
-        $req =new HTTP_Request($url);
-        $req->setMethod(HTTP_REQUEST_METHOD_GET);
-        if (isset($params["auth_data"]) && is_array($params["auth_data"])) {
-            $req->setBasicAuth($params["auth_data"]["login"], $params["auth_data"]["pass"]);
-        }
-        if (is_array($params["request_data"])) {
-            foreach ($params["request_data"] as $var=>$val){
-                $req->addQueryString($var, $val);
-            }
-        }
-
-        if (is_array($params["headers"])) {
-            foreach ($params["headers"] as $var=>$val){
-                $req->addHeader($var, $val);
-            }
-        }
+       return $aRet;
+   }
 
 
-        if (!PEAR::isError($req->sendRequest())) {
-            return $req->getResponseBody();
-        }
-    }
+   public function getURL($url, $params=array()){
+       //syslog(LOG_DEBUG, "getURL: $url");
+       require_once("HTTP/Request.php");
+       $req =new HTTP_Request($url);
+       $req->setMethod(HTTP_REQUEST_METHOD_GET);
+       if (isset($params["auth_data"]) && is_array($params["auth_data"])) {
+           $req->setBasicAuth($params["auth_data"]["login"], $params["auth_data"]["pass"]);
+       }
+       if (is_array($params["request_data"])) {
+           foreach ($params["request_data"] as $var=>$val){
+               $req->addQueryString($var, $val);
+           }
+       }
 
-    private function postURL($url, $params=array()){
-            require_once("HTTP/Request.php");
-            $req = new HTTP_Request($url);
-            $req->setMethod(HTTP_REQUEST_METHOD_POST);
-    
-            if (!$params["options"]["no-multipart"]) {
-                $req->addHeader("Content-type","multipart/form-data");
-            }
-            if (preg_match("/http:\/\/([^\/]*)/", $url, $match)){
-                $host=$match[1];
-                $req->addHeader("Host",$host);
-            }
-    
-    
-            if (is_array($params["headers"])) {
-                foreach ($params["headers"] as $var=>$val){
-                    $req->addHeader($var, $val);
-                }
-            }
-    
-            if (is_array($params["request_data"])) {
-    
-                foreach ($params["request_data"] as $var=>$val){
-                    $req->addPostData($var, $val);
-                }
-            }
-    
-            $req->addPostData("Sender", "postURL");
-    
-    
-            if (!PEAR::isError($req->sendRequest())) {
-                return $req->getResponseBody();
-            }
-    }
+       if (is_array($params["headers"])) {
+           foreach ($params["headers"] as $var=>$val){
+               $req->addHeader($var, $val);
+           }
+       }
 
+
+       if (!PEAR::isError($req->sendRequest())) {
+           return $req->getResponseBody();
+       }
+   }
+
+   private function postURL($url, $params=array()){
+           require_once("HTTP/Request.php");
+           $req = new HTTP_Request($url);
+           $req->setMethod(HTTP_REQUEST_METHOD_POST);
+   
+           if (!$params["options"]["no-multipart"]) {
+               $req->addHeader("Content-type","multipart/form-data");
+           }
+           if (preg_match("/http:\/\/([^\/]*)/", $url, $match)){
+               $host=$match[1];
+               $req->addHeader("Host",$host);
+           }
+   
+   
+           if (is_array($params["headers"])) {
+               foreach ($params["headers"] as $var=>$val){
+                   $req->addHeader($var, $val);
+               }
+           }
+   
+           if (is_array($params["request_data"])) {
+   
+               foreach ($params["request_data"] as $var=>$val){
+                   $req->addPostData($var, $val);
+               }
+           }
+   
+           $req->addPostData("Sender", "postURL");
+   
+   
+           if (!PEAR::isError($req->sendRequest())) {
+               return $req->getResponseBody();
+           }
+   }
 
 }
