640961f53ff94a7d0100745999849802293d8216
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-11-17 15:21:04 +0100

    Adding example for local/http transport for the BLS BE

--- bin/skel/config/config.template.php
+++ bin/skel/config/config.template.php
@@ -1,5 +1,6 @@
 <?php
 ini_set("date.timezone", 'Europe/Berlin');
+ini_set("display_errors", 'Off'); // Off for production and On for development
 
 /** Configuration Variables **/
 
@@ -8,8 +9,16 @@ define ('DEVELOPMENT_ENVIRONMENT',true);
 
 define('BASE_URL','/%%appname%%');
 
-define('BLSERVER_URL','http://localhost/tipibls');
-define('BLSERVER_SHARED_KEY','s0m3p433');
+# use this for development. Makes a direct connections to the BLS backend
+define('BLSEBE_TRANSPORT','local'); 
+define('BLSBE_PROJECT_HOME','/Users/aghaffar/www/sanbls'); 
+define('BLSBE_BOOTSTRAP_FILE','/usr/local/blsbe/library/bootstrap.php'); 
+
+# use this for production. The backend server shoud be on a different host / load balancer
+# define('BLSEBE_TRANSPORT','http'); 
+# define('BLSERVER_URL','http://someserver:9090');
+# define('BLSERVER_SHARED_KEY','s0m3p433');
+
 
 define('CACHE_BACKEND', 'FILE');
 define('CACHE_PATH', ROOT . DS . "tmp" . DS . "cache");
@@ -26,24 +35,24 @@ define('DATE_FORMAT_DE', "j/n/Y, H:i:s");
 define('TEMPLATE_BACKEND', "Smarty");
 
 
-//smarty options
+//smarty options, defaults are ok
 
 // Path to Smarty.class.php
-define('SMARTY_LIBRARY', ROOT.DS."library".DS."3rdparty".DS."smarty".DS."Smarty-3.0.8".DS."libs".DS."Smarty.class.php");
+// define('SMARTY_LIBRARY', ROOT.DS."library".DS."3rdparty".DS."smarty".DS."Smarty-3.0.8".DS."libs".DS."Smarty.class.php");
 
 // Default template directory 
-define('SMARTY_TEMPLATE_DIR', ROOT.DS."application".DS."views");
+// define('SMARTY_TEMPLATE_DIR', ROOT.DS."application".DS."views");
 
 
-define('SMARTY_COMPILE_DIR', ROOT.DS."tmp".DS."smarty_compile");
-define('SMARTY_CACHE_DIR', ROOT.DS."tmp".DS."smarty_cache");
+// define('SMARTY_COMPILE_DIR', ROOT.DS."tmp".DS."smarty_compile");
+// define('SMARTY_CACHE_DIR', ROOT.DS."tmp".DS."smarty_cache");
 
 // YOUR smarty plugins directory. This will be added to the list of smarty plugins
-define('SMARTY_LOCAL_PLUGINS_DIR', ROOT.DS."library".DS."smarty-plugins");
+// define('SMARTY_LOCAL_PLUGINS_DIR', ROOT.DS."library".DS."smarty-plugins");
 
 // left and right delimeters for smarty tags. we recommend <{ $something }>
-define('SMARTY_LEFT_DELIMETER', "<{");
-define('SMARTY_RIGHT_DELIMETER', "}>");
+// define('SMARTY_LEFT_DELIMETER', "<{");
+// define('SMARTY_RIGHT_DELIMETER', "}>");
 
 
 //stuff related to tipiness v4->v5
