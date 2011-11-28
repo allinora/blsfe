8c6b0b5f171c7563ca6a6a6ca75588cf9b6046c5
Atif Ghaffar <atif.ghaffar@gmail.com>
2011-11-28 11:12:21 +0100

    Use office2003 skin

--- assets/admin/ckeditor.js
+++ assets/admin/ckeditor.js
@@ -7,5 +7,5 @@ CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	config.skin='v2';
+	config.skin='office2003';
 };
