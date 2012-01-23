d1b09fa75cc6c7ed9bf29d98367a0b0df7936abe
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-01-23 23:24:14 +0100

    Return empty png if the image does not exists

--- helpers/cmsImage.php
+++ helpers/cmsImage.php
@@ -3,13 +3,19 @@
 function blsfe_helper_cmsImage($id, $x=0, $y=0){
 	$model=new BLModel("/sys/image", "id");
 	$image=$model->get($id);
-	if (!$x){
-		$x=0;
+	
+	if (is_array($image) && $image["name"]){
+		
+		if (!$x){
+			$x=0;
+		}
+		if (!$y){
+			$y=0;
+		}
+		return "<img src='/core/images/cms/system/$id/$x/$y/" . $image["name"] . "'>";
 	}
-	if (!$y){
-		$y=0;
-	}
-	return "<img src='/core/images/cms/system/$id/$x/$y/" . $image["name"] . "'>";
-	print "<pre>" . print_r($image, true) . "</pre>";
+	return "<img src='/fwassets/images/1x1.png' width='$x' height='$y'>";
+	
+	// print "<pre>" . print_r($image, true) . "</pre>";
 };
 
