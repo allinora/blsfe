3f6b746cc01784b1f05158b89fbc30cfc63f9483
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-01-29 18:41:44 +0100

    Adding BaseController

---
+++ bin/skel/application/controllers/BaseController.php
@@ -0,0 +1,27 @@
+<?php
+
+class BaseController extends App_Controller {
+
+	private function checkSession(){
+		// Setup the session etc here
+        if ($_SESSION["loginFB"]){
+			parent::facebooklogin();
+		}
+		if ($_REQUEST["op"]=="logout"){
+			parent::logout();
+		}
+		if ($_REQUEST["op"]=="login"){
+			parent::login();
+		}
+	}
+	function beforeAction () {
+		parent::beforeAction();
+		$this->checkSession();
+	}
+
+	function indexAction() {
+	}
+
+	function afterAction() {
+	}
+}
--- bin/skel/application/controllers/indexcontroller.php
+++ bin/skel/application/controllers/indexcontroller.php
@@ -1,6 +1,7 @@
 <?php
 
-class IndexController extends BLController {
+include_once(dirname(__FILE__) . "/BaseController.php");
+class IndexController extends BaseController{
 
 	
 	function indexAction() {
