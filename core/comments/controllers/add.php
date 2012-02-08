cc6ef47d4db025023917809c5a710bcbef34f616
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-02-08 12:51:39 +0100

    Use user_id instead of id

--- core/comments/controllers/add.php
+++ core/comments/controllers/add.php
@@ -10,9 +10,9 @@ class Core_Comments_AddController extends Core_Comments_Controller {
 	function indexAction() {
 		global $_SESSION;
 		$this->render=0;
-		if ($_REQUEST["object_type"] && $_REQUEST["object_id"] && $_SESSION["user"]["id"] && $_REQUEST["comment"]){
+		if ($_REQUEST["object_type"] && $_REQUEST["object_id"] && $_SESSION["user"]["user_id"] && $_REQUEST["comment"]){
 			$_params=$_REQUEST;
-			$_params["user_id"]=$_SESSION["user"]["id"];
+			$_params["user_id"]=$_SESSION["user"]["user_id"];
 			$x=$this->model->add($_params);
 			print $x;
 		} else {
