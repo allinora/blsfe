47d32f521e17a3176e25789fd5db7fa05b2db705
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-02-13 23:41:51 +0100

    Some more fixing

--- core/projectofferentries/controllers/admin.php
+++ core/projectofferentries/controllers/admin.php
@@ -16,14 +16,17 @@ class Core_Projectofferentries_AdminController extends Admin_Controller {
 
 	function formatters(){
 		$formatters=array();
+		$formatters["offer_id"]["value"]=$_REQUEST["offer_id"];
+		$formatters["offer_id"]["formtype"]="hidden_with_value";
+		$formatters["description"]["css"]="width:500px; height: 70px;";
 		return $formatters;
 	}
 
 	function addAction() {
-		parent::addAction("core/". $this->tab. "/admin");
+		parent::addAction("core/". $this->tab. "/admin/index/" . $_REQUEST["offer_id"]);
 	}
 	
 	function editAction($id) {
-		parent::editAction($id, "core/". $this->tab. "/admin");
+		parent::editAction($id, "core/". $this->tab. "/admin/index/" . $_REQUEST["offer_id"]);
 	}
 }
