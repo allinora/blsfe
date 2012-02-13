e2d12ec6eb26f484b8aed4eca704e26177ddf564
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-02-13 20:11:57 +0100

    Project offers

---
+++ core/projectoffers/controllers/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+class Core_Projectoffers_AdminController extends Admin_Controller {
+
+	function beforeAction(){
+		parent::beforeAction();
+		$this->tab=$this->setTabName(__CLASS__);
+		//$this->set("tab", $this->tab);
+		$this->model=new BLModel("sys/pm/project/offer", "id", "company_id");
+	}
+	function indexAction() {
+		$res=$this->model->getAllOffers();
+		$this->set("aData", $res);
+	}
+	
+
+	function formatters(){
+		$formatters=array();
+		$formatters["company_id"]["helper"]="blsfe_helper_companyList";
+		$formatters["description"]["css"]="width:600px; height: 400px;";
+		return $formatters;
+	}
+
+	function addAction() {
+		parent::addAction("core/". $this->tab. "/admin");
+	}
+	
+	function editAction($id) {
+		parent::editAction($id, "core/". $this->tab. "/admin");
+	}
+}
