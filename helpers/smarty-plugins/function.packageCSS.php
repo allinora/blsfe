4eb721b9ef23bd0eb1e86f3c4d53f6d8d3dfb9e1
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-02-21 01:05:02 +0100

    Avoiding using the same file twice

--- helpers/smarty-plugins/function.packageCSS.php
+++ helpers/smarty-plugins/function.packageCSS.php
@@ -5,10 +5,16 @@ function smarty_function_packageCSS($params, &$smarty){
 	if (!count($packages)){
 		return;
 	}
+	$seen=array(); // Array to not load the same file more than once
 	foreach($packages as $p){
 		if (count($p["css"])>0){
 			foreach($p["css"] as $css){
+				if ($seen[$css]){
+					continue;
+				}
+				$seen[$css]++;
 				$html.='<link rel="stylesheet" type="text/css" href="'.$css.'" />' . "\n";
+				
 			}
 		}
 	}
--- helpers/smarty-plugins/function.packageJS.php
+++ helpers/smarty-plugins/function.packageJS.php
@@ -5,9 +5,14 @@ function smarty_function_packageJS($params, &$smarty){
 	if (!count($packages)){
 		return;
 	}
+	$seen=array(); // Array to not load the same file more than once
 	foreach($packages as $p){
 		if (count($p["js"])>0){
 			foreach($p["js"] as $js){
+				if ($seen[$js]){
+					continue;
+				}
+				$seen[$js]++;
 				$html.='<script type="text/javascript" src="'.$js.'"></script>' . "\n";
 			}
 		}
