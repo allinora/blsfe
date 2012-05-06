040681fc3ba61abbfd130e394b29a6035da66a5d
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-05-06 15:10:58 +0200

    Moving the systemImage function to imagemanager

--- assets/js/adminbootstrap.js
+++ assets/js/adminbootstrap.js
@@ -28,10 +28,3 @@ $(function() {
 	});
 
 });
-
-function systemImage(lyr,id,url){
-	//$('#fancybox-close').show();
-    $('#fancybox-close').trigger('click');
-	$('#'+lyr).val(id);
-	$('#systemImageManager-'+lyr).html('<img src="'+url+'/100">');  // Another thumb
-}
