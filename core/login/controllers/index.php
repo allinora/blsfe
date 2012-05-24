7d28e1d4b54f526c1646e12a2cfb31f56290bf25
Atif Ghaffar <atif.ghaffar@gmail.com>
2012-05-24 15:20:09 +0200

    Use the user session

--- core/login/controllers/index.php
+++ core/login/controllers/index.php
@@ -10,11 +10,9 @@ class Core_Login_Controller extends Core_Controller {
 	function indexAction() {
 		$this->set("noMenu", 1);
 		if ($_SERVER["REQUEST_METHOD"] == "POST"){
-			$x=$this->model->login(array("email"=>$_REQUEST["login"], "passwd"=>$_REQUEST["passwd"]));
-			//print "<pre>r" . print_r($_REQUEST, true) . "</pre>";
-			//print "<pre>x" . print_r($x, true) . "</pre>";
+			$x=$this->model->login(array("email"=>$_REQUEST["email"], "passwd"=>$_REQUEST["passwd"]));
 			if ($x["user_id"]>0){
-				$_SESSION["authdata"]=$x;
+				$_SESSION["user"]=$x;
 				$this->redirect("core", "admin");
 			}
 		}
