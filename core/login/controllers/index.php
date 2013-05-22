a732f4df0b6a0e129bfad750f23d7f18209b27eb
Atif Ghaffar <atif.ghaffar@gmail.com>
2013-05-22 11:48:49 +0200

    Use the admin controller instead of user controller

--- core/login/controllers/index.php
+++ core/login/controllers/index.php
@@ -4,17 +4,23 @@ class Core_Login_Controller extends Core_Controller {
 
 	function beforeAction(){
 		parent::beforeAction();
-		$this->model=new BLModel("sys/auth/user", "id");
+		$this->model=new BLModel("sys/auth/admin", "id");
 		
 	}
 	function indexAction() {
 		$this->set("noMenu", 1);
 		if ($_SERVER["REQUEST_METHOD"] == "POST"){
-			$x=$this->model->login(array("email"=>$_REQUEST["email"], "passwd"=>$_REQUEST["passwd"]));
-			if ($x["user_id"]>0){
+			$x=$this->model->login(array("login"=>$_REQUEST["login"], "passwd"=>$_REQUEST["passwd"]));
+			if ($x["id"]>0 && $x["active"]>0){
 				$_SESSION["user"]=$x;
 				$this->redirect("core", "admin");
 			}
+			/*
+			print "<pre>REQUEST: " . print_r($_REQUEST, true) . "</pre>";
+			print "<pre>RESPONSE: " . print_r($x, true) . "</pre>";
+			print "<pre>SESSION: " . print_r($_SESSION, true) . "</pre>";
+			exit;
+			*/
 		}
 		
 	}
