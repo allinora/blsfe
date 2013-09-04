da21b4ef2e877db3f1a9d6465b32013fa0967a51
Atif Ghaffar <atif.ghaffar@gmail.com>
2013-09-04 13:56:16 +0200

    Adding projectfolders

--- core/projectfolders/controllers/admin.php
+++ core/projectfolders/controllers/admin.php
@@ -26,4 +26,39 @@ class Core_Projectfolders_AdminController extends Admin_Controller {
 	function editAction($id) {
 		parent::editAction($id, "core/". $this->tab. "/admin");
 	}
+
+
+
+
+	function indexAction() {
+		$res=$this->model->getAllEntries();
+		$this->set("aData", $res);
+	}
+
+	function listAction($id) {
+		$res=$this->model->getall($id);
+		$this->set("aData", $res);
+	}
+	
+
+	function formatters(){
+		$formatters=array();
+		$formatters["service_id"]["helper"]="blsfe_helper_pmserviceList";
+		$formatters["project_id"]["helper"]="blsfe_helper_pmprojectList";
+		$formatters["user_id"]["helper"]="blsfe_helper_userList";
+		$formatters["status"]["helper"]="blsfe_helper_pmstatusList";
+		$formatters["start_time"]["class"]="datetimepicker";
+		$formatters["end_time"]["class"]="datetimepicker";
+		$formatters["description"]["css"]="width: 600px; height: 250px;";
+		
+		return $formatters;
+	}
+
+	function addAction() {
+		parent::addAction("core/". $this->tab. "/admin");
+	}
+	
+	function editAction($id) {
+		parent::editAction($id, "core/". $this->tab. "/admin");
+	}
 }
