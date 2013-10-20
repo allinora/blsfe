99b2330ac572eda6df544228489ae840125b40a3
Atif Ghaffar <atif.ghaffar@gmail.com>
2013-10-20 11:24:21 +0200

    formatting

--- library/cache.class.php
+++ library/cache.class.php
@@ -12,13 +12,13 @@ class Cache {
 			return self::$instance;
         }
 		
-		$backend=CACHE_BACKEND;
-		$backend_driver_file=dirname(__FILE__) . DS . strtolower(__CLASS__) . DS . strtolower($backend) . ".class.php";
+		$backend = CACHE_BACKEND;
+		$backend_driver_file = dirname(__FILE__) . DS . strtolower(__CLASS__) . DS . strtolower($backend) . '.class.php';
 		if (file_exists($backend_driver_file)){
 			include_once($backend_driver_file);
-			$class_name="Cache_" . ucfirst(strtolower($backend));
+			$class_name = "Cache_" . ucfirst(strtolower($backend));
 			if (class_exists($class_name)){
-				self::$instance=new $class_name;
+				self::$instance = new $class_name;
 				return self::$instance;
 			} else {
 				die($class_name . " class does not exists");
