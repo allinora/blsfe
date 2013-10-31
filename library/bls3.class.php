58de4b641ecdbdfbd50e7a7916b2ec1f54f322fd
Atif Ghaffar <atif.ghaffar@gmail.com>
2013-10-31 13:10:34 +0100

    Adding s3

---
+++ library/bls3.class.php
@@ -0,0 +1,19 @@
+<?php
+include_once("/opt/allinora/blsbe/library/3rdparty/S3.php");
+class BLS3 extends S3 {
+	
+	public function __construct($accessKey = null, $secretKey = null, $useSSL = false, $endpoint = 's3.amazonaws.com'){
+		if (defined('STORAGE_SERVER_HOST') && defined('STORAGE_SERVER_ACCESS_KEY') && defined('STORAGE_SERVER_SECRET_KEY')){
+			
+			$endpoint = STORAGE_SERVER_HOST;
+			$accessKey = STORAGE_SERVER_ACCESS_KEY;
+			$secretKey = STORAGE_SERVER_SECRET_KEY;
+			return parent::__construct($accessKey, $secretKey, $useSSL, $endpoint);
+		} else {
+			throw new Exception("Some constants are not correctly defined. Cannot use Storage");
+		}
+
+	}
+
+}
+
