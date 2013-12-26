2a191c7ee75b112bd2c516ecc69192c61961b101
Atif Ghaffar <atif.ghaffar@gmail.com>
2013-12-26 23:02:32 +0100

    simplify

--- bin/skel/public/index.php
+++ bin/skel/public/index.php
@@ -1,64 +1,33 @@
 <?php	
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
+ini_set("display_errors", "On");
+ini_set("display_startup_errors", "On");
+ini_set("html_errors", "On");
+
+ini_set("zlib.output_compression", "Off");
+date_default_timezone_set("Europe/Zurich");
 
 
-// Possible location where we can find the BLSFE library
-$blsfe_locations=array(
-        '/opt/git/blsfe',
-        "/Users/aghaffar/www/blsfe",
-        "/home/tipi/blsfe",
-		"/usr/local/blsfe",
-		"/www/blsfe"
-);
-
-$_blsfe_bootstrap_file=_findBootstrap($blsfe_locations) . "/library/bootstrap.php";
+define("CACHE_DISABLED", false);
 
 
 
-// Do something before the main Hook is called
-function _app_preHook(){
-	if (in_array($_REQUEST["op"], array("fblogin", "fbsignup2"))){
-    include(dirname(__FILE__) . "/login.php");
-    exit;
-    }
-    if ($_SESSION["fb_user_data"]){
-            $_SESSION["loginFB"]=($_SESSION["loginFB"])?++$_SESSION["loginFB"]:1;
-    }
-    
-}
-
-
-// Do something with the content just before its being printed.
 function _app_contentHook($_content){
-	blsfe_load_class("BLTranslate");
-	$translator=new BLTranslate(LANG, "ProjectName");
+        blsfe_load_class("BLTranslate");
+        $translator = new BLTranslate(LANG, "wbs_widgets");
+        // Do something to the content before printing it out.
+        $data = preg_replace_callback("@<po>([^<]*)?</po>@", array($translator, "translate"), $_content);
 
-
-	// Do something to the content before printinga it out.
-	$data=preg_replace_callback("@<po>([^<]*)?</po>@", array($translator, "translate"), $_content);
-	print $data;
+		if (!DEVELOPMENT_ENVIRONMENT){
+			// Rewrite all /fwassets url to the static server	
+		 	$static_server = (defined('CMS_STATIC_URL')) ? CMS_STATIC_URL : '//static.worldsoft-wbs.com';
+			$data = preg_replace("@\"/fwassets/@", '"' . $static_server . '/assets/', $data);
+		}
+        print $data;
 }
 
 
-
-function _findBootstrap($blsfe_locations){
-	foreach($blsfe_locations as $l){
-		if (file_exists($l)){
-			return $l;
-	    }
-	}
-	
-}
-
-if (isset($_SERVER["BLSFE_BOOTSTRAP_FILE"])){
-        // This is for development. 
-        if (file_exists($_SERVER["BLSFE_BOOTSTRAP_FILE"])){
-                require_once($_SERVER["BLSFE_BOOTSTRAP_FILE"]);
-        }
-} else {
-        if (file_exists($_blsfe_bootstrap_file)){
-                require_once($_blsfe_bootstrap_file);
-        }
-}
+$_blsfe_bootstrap_file='/opt/allinora/blsfe//library/bootstrap.php';
+require_once($_blsfe_bootstrap_file);
 
