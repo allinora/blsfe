a11cf3eb25160777d151058a2f87eeffe631f25e
Atif Ghaffar <atif.ghaffar@gmail.com>
2013-12-27 12:50:57 +0100

    helper to make arbitary select list like for status

---
+++ helpers/selectList.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function blsfe_helper_selectList($field_name, $items=array(), $id=''){
+	
+	$text="<select name='$field_name' id='$field_name'>";
+	if (is_array($items)) {
+		foreach($items as $item){
+			$text.="<option value='" . $item . "'";
+			if ($id == $item){
+				$text.=" selected ";
+			}
+			$text.=">" . $item . "</option>";
+		}
+	}
+	$text.="</select>";
+	return $text;
+}
+
