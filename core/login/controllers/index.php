98e4c0f4f773b283a366ce4c235f81f77156659c
Atif Ghaffar <atif.ghaffar@gmail.com>
2014-03-01 19:53:22 +0100

    Adding canvas admin and making changes accordingly

--- core/login/controllers/index.php
+++ core/login/controllers/index.php
@@ -10,6 +10,10 @@ class Core_Login_Controller extends Core_Controller {
 	function indexAction() {
 		$this->set("noMenu", 1);
 		$this->doNotRenderHeader=0;
+		
+		$_wrapper_directory = __DIR__ . "/../views";
+		$this->setWrapperDir($_wrapper_directory);
+		
 		if ($_SERVER["REQUEST_METHOD"] == "POST"){
 			$x=$this->model->login(array("login"=>$_REQUEST["login"], "passwd"=>$_REQUEST["passwd"]));
 			if ($x["id"]>0 && $x["active"]>0){
