26bdc8b2b2e2b541b9848b9e2ff350135254d22f
Atif Ghaffar <atif.ghaffar@gmail.com>
2014-03-15 14:27:26 +0100

    Use env variables instead of CONSTANTS

--- library/bls3.class.php
+++ library/bls3.class.php
@@ -3,17 +3,15 @@ include_once(__DIR__ . "/3rdparty/S3.php");
 class BLS3 extends S3 {
 	
 	public function __construct($accessKey = null, $secretKey = null, $useSSL = false, $endpoint = 's3.amazonaws.com'){
-		if (defined('STORAGE_SERVER_HOST') && defined('STORAGE_SERVER_ACCESS_KEY') && defined('STORAGE_SERVER_SECRET_KEY')){
-			
-			$endpoint = STORAGE_SERVER_HOST;
-			$accessKey = STORAGE_SERVER_ACCESS_KEY;
-			$secretKey = STORAGE_SERVER_SECRET_KEY;
+		if (isset($_ENV['STORAGE_SERVER_HOST']) && isset($_ENV['STORAGE_SERVER_ACCESS_KEY']) && isset($_ENV['STORAGE_SERVER_ACCESS_KEY'])){
+			$endpoint = $_ENV['STORAGE_SERVER_HOST'];
+			$accessKey = $_ENV['STORAGE_SERVER_ACCESS_KEY'];
+			$secretKey = $_ENV['STORAGE_SERVER_SECRET_KEY'];
 			return parent::__construct($accessKey, $secretKey, $useSSL, $endpoint);
 		} else {
-			throw new Exception("Some constants are not correctly defined. Cannot use Storage");
+			throw new Exception("Some ENV variables are not correctly defined. Cannot use Storage");
 		}
 
 	}
-
 }
 
