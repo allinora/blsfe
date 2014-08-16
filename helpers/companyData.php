d23d9965b8b65cb51bd2ccb033c18a4271e4bd32
Atif Ghaffar <atif.ghaffar@gmail.com>
2014-08-17 01:59:45 +0200

    logic for country.name

--- helpers/companyData.php
+++ helpers/companyData.php
@@ -1,10 +1,20 @@
 <?php
 
 function blsfe_helper_companyData($id, $field){
-	$model=new BLModel("/sys/company", "id");
-	$aData=$model->get($id);
-		if ($aData[$field]){
-			return $aData[$field];
+	$model = new BLModel('/sys/company', 'id');
+	$aData = $model->get($id);
+	
+	if ($field == 'country.name'){
+		$m = new BLModel('sys/country');
+		$country = $m->getByCode(array('code' => $aData['country']));
+		if (is_array($country)) {
+			return $country['name'];
 		}
+	}
+
+
+	if ($aData[$field]){
+		return $aData[$field];
+	}
 };
 
